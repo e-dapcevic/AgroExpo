9f40634c06bf04403eea12b00e13a78645755e66
e-dapcevic <55240581+e-dapcevic@users.noreply.github.com>
2020-03-23 19:09:02 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import React, { Component } from "react";
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoadingPage from './LoadingPage'
-import DropdownPage from './DropdownPage'
-import CameraPermission from './CameraPermission'
-import GifPage from './GifPage'
-import MenuPage from './MenuPage'
+//import MenuPage from './MenuPage';
+import GifPage from './GifPage';
 
 
 const Stack = createStackNavigator();
@@ -21,15 +17,15 @@ const MyTheme = {
 
 function App() {
   return (
-    <NavigationContainer  theme={MyTheme}>
-      <Stack.Navigator initialRouteName="LandingPage" screenOptions={{
+    <NavigationContainer theme={MyTheme}>
+      <Stack.Navigator initialRouteName="GifPage" screenOptions={{
         headerShown: false
       }}>
-        <Stack.Screen name="LoadingPage" component={LoadingPage} />
-        <Stack.Screen name="DropdownPage" component={DropdownPage} />
-        <Stack.Screen name="CameraPermission" component={CameraPermission} />
+        {/* <Stack.Screen name="LoadingPage" component={LoadingPage} /> */}
+        {/* <Stack.Screen name="DropdownPage" component={DropdownPage} /> */}
+        {/* <Stack.Screen name="CameraPermission" component={CameraPermission} /> */}
         <Stack.Screen name="GifPage" component={GifPage}/>
-        {/* <Stack.Screen name="MenuPage" component={MenuPage}/> */}
+        {/* <Stack.Screen name="GifPage" component={GifPage}/> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
